a5894d8965586d6dc379fef1f9a641bd5b5dc9c8
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-05-08 23:12:31 -0300

    dominio con store, no anda

--- sgat/dominio/dominio-talleres.js
+++ sgat/dominio/dominio-talleres.js
@@ -1,5 +1,5 @@
-const Store = require('./store')
-const store = Store.llenar()
+
+const store = new Store().llenar()
 
 //(NO ACCEDER DIRECTO A LAS PROPIEDADES)
 class Taller {
@@ -244,4 +244,66 @@ function indiceDeLista(elemento, list){
     return list.findIndex(elemento)
 }
 
-exports.dominio = { Taller, Persona, Curso, DiaHorarioLugar };
+class Store {
+    constructor(){
+        this.categorias = []
+        this.cursos = []
+        this.talleres = []
+        this.lugares = []
+    }
+
+    addCategoria(cat){
+        if(!this.getCategorias().some((c)=> cat == c)){
+            this.categorias.push(cat)
+            //this.ordenarCategorias()
+        }
+    }
+    //Borrar Categoria deberia checkear que ningun taller la este usando.
+
+    addTaller(taller){
+        this.addCategoria(taller.getCategoria())
+        this.talleres.push(taller)
+        return taller
+    }
+    
+    getTalleres(){ return this.talleres }
+
+    getCategorias(){return this.categorias}
+
+    getCategoriaLlamada(nombre){return this.categorias.find((c)=> c == nombre)}
+
+    llenar(){
+        this.categorias = ['Artes Manuales','Instrumentos Musicales']
+
+        const cat1 = this.getCategoriaLlamada('Artes Manuales')
+        const cat2 = this.getCategoriaLlamada('Instrumentos Musicales')
+        
+        //Agrega talleres de Artes Manuales
+        let ceramica = this.addTaller(new Taller(cat1,'Ceramica','Normal','Aborigen'))
+        this.addTaller(new Taller(cat1,'Tallado En Madera'))
+        this.addTaller(new Taller(cat1,'Mimbreria'))
+        this.addTaller(new Taller(cat1,'Plateria'))
+        this.addTaller(new Taller(cat1,'Marroquineria'))
+        this.addTaller(new Taller(cat1,'Dibujo', 'Normal', 'Digital'))
+        this.addTaller(new Taller(cat1,'Arte Juvenil'))
+
+        //Agrega talleres de instrumentos Musicales
+        this.addTaller(new Taller(cat2,'Piano','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Bajo','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Guitarra','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Violín','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Viola','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Violoncello','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Contrabajo','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Bateria','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Bandoneon','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Flauta Traversa','Principiantes','Avanzados'))
+        this.addTaller(new Taller(cat2,'Piano','Principiantes','Avanzados'))
+    
+        let prof1 = new Persona(12345678,'Juan','Perez','12/02/1980','',2243451234,'a@a.com')
+        this.addCurso(new Curso(10,ceramica.getSubCategoria('Normal'),prof1))
+    }
+}
+
+
+exports = {dominio:{ Taller, Persona, Curso, DiaHorarioLugar },Store}
