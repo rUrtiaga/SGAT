2706a6927a4984756505974178e343dc1a538c5c
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-05-17 18:04:49 -0300

    en proceso 2

--- sgat/src/nuevoAlumno.js
+++ sgat/src/nuevoAlumno.js
@@ -1,7 +1,14 @@
- const React = require('react')
+const React = require('react')
+const axios = require('axios')
 
 class NuevoAlumno extends React.Component{
-
+    constructor(props){
+        super(props)
+        this.state = {
+            persona: new Persona(),
+            curso: null
+        }
+    }
     render(){
         return (
             <div className="container">
@@ -16,13 +23,58 @@ class NuevoAlumno extends React.Component{
                         <option>Tallado en madera</option>
                     </select>
                 </div>
-                 <InputPersona/>
+                <InputPersona id='inputPersona' persona={this.state.persona} padre={this}/>
             </div>
         )
     }
+
+    limpiar(){
+        console.log(this.props.children)
+    }
+
 }
 
+
 class InputPersona extends React.Component{
+    constructor(props){
+        super(props)
+        this._persona = this.props.persona
+        this.state = {
+            dni: persona.dni,
+            nombre: persona.nombre,
+            apellido: persona.apellido,
+            direccion: persona.direccion,
+            telPrincipal: persona.telPrincipal,
+            telSecundario: persona.telSecundario,
+            mail: persona.mail,
+            comentario: persona.comentario,
+        }
+    }
+
+    handleChange(event) {
+        try {
+            this.validate(event.target.name,event.target.value)
+            this.setState({[event.target.name] : event.target.value  });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    validate(nameEvent,value){
+        switch (nameEvent) {
+            case 'dni':
+                if(value > 100000000){
+                    throw new Error('El dni ingresado es demasiado alto')
+                }
+                if(value.toString().match(/[^0-9]+$/) ){
+                    throw new Error('Ingrese solo numeros')
+                }
+                break;
+ 
+            default:
+                break;
+        }
+    }
 
     render(){
         return(
@@ -31,7 +83,7 @@ class InputPersona extends React.Component{
                     <div className="form-row">
                         <div className="col">
                             <label htmlFor="name">D.N.I.</label>
-                            <input type="number" className="form-control" id="name" placeholder="D.N.I."/>
+                            <input type="text" className="form-control" name="dni" placeholder="D.N.I." value={this.state.dni} onChange={(event) => this.handleChange(event)}/>
                         </div>
                     </div>
                 </div>
@@ -40,49 +92,76 @@ class InputPersona extends React.Component{
                     <div className="form-row">
                         <div className="col">
                             <label htmlFor="name">Nombre</label>
-                            <input type="text" className="form-control" id="name" placeholder="introduzca Nombre"/>
+                            <input type="text" className="form-control" name="nombre" placeholder="introduzca Nombre" value={this.state.nombre} onChange={(event)=> this.handleChange(event)}/>
                         </div>
                         <div className="col">
                             <label htmlFor="lastname">Apellido</label>
-                            <input type="text" className="form-control" id="lastname" placeholder="introduzca Apellido"/>
+                            <input type="text" className="form-control" name="apellido" placeholder="introduzca Apellido" value={this.state.apellido} onChange={(event)=> this.handleChange(event)}/>
                         </div>
                     </div>
                 </div>
 
                 <div className="form-group">
                     <h4 className="mt-4 mb-4" htmlFor="contact">Contacto</h4>
+                    <div  className="form-group">
+                            <label htmlFor="direc">Dirección</label>
+                            <input type="text" className="form-control" name="direccion" placeholder="ingrese direccion"value={this.state.direccion} onChange={(event)=> this.handleChange(event)}/>
+                    </div>
                     <div className="form-row">
                         <div className="col">
                             <label htmlFor="pPhone">Telefono Principal</label>
-                            <input type="number" className="form-control" id="phone" placeholder="formato: 0224345XXXX"/>
+                            <input type="number" className="form-control" name="telPrincipal" placeholder="formato: 0224345XXXX" value={this.state.telPrincipal} onChange={(event)=> this.handleChange(event)}/>
                         </div>
                         <div className="col">
                             <label htmlFor="sPhone">Secundario</label>
-                            <input type="number" className="form-control" id="aphone" placeholder="formato: 0224345XXXX"/>
+                            <input type="number" className="form-control" name="telSecundario" placeholder="formato: 0224345XXXX" value={this.state.telSecundario} onChange={(event)=> this.handleChange(event)}/>
                         </div>
                     </div>
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="exampleFormControlInput1">Correo Electronico</label>
-                    <input type="email" className="form-control" id="email" placeholder="nombre@dominio.com"/>
+                    <input type="email" className="form-control" name="mail" placeholder="nombre@dominio.com" value={this.state.mail} onChange={(event)=> this.handleChange(event)}/>
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="exampleFormControlTextarea1">Comentario</label>
-                    <textarea className="form-control" id="comment" rows="3"></textarea>
+                    <textarea className="form-control" name="comentario" rows="3" value={this.state.comentario} onChange={(event)=> this.handleChange(event)}></textarea>
                 </div>
                 <div className="row justify-content-end">
                     <div className="col-md-2">
-                        <button type="submit" className='btn btn-danger'>Cancelar</button>
+                        <button type="submit" className='btn btn-danger' onClick={()=>this.cancel()}>Cancelar</button>
                     </div>
                     <div className="col-md-2">
-                        <button type="submit" className='btn btn-primary'>Aceptar</button>
+                        <button type="submit" className='btn btn-primary' onClick={()=>this.aceptarAlumno()} >Aceptar</button>
                     </div>
                 </div>
+
             </React.Fragment>
         )
+    }
+    cancel(){
+        this.limpiar()
+        this.props.padre.cancel()
+    }
+
+    limpiar(){
+        this.setState({
+            dni: "",
+            nombre: "",
+            apellido: "",
+            direccion: "",
+            telPrincipal: "",
+            telSecundario: "",
+            mail: "",
+            comentario: "",
+        })
+    }
+
+    aceptarAlumno(){
+
     }
 }
 
 exports.NuevoAlumno= NuevoAlumno
+exports.InputPersona = InputPersona
--- sgat/src/sgat.js
+++ sgat/src/sgat.js
@@ -1,6 +1,4 @@
 const React = require('react')
-const ReactDOM = require('react-dom')
-
 
 // variables para componentes de pantallas
 const nuevoTaller = require('./nuevoTallerReact')
@@ -49,34 +47,30 @@ class Menu extends React.Component {
     menuBarra(){
         return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand" href="#" >S.G.A.T.</a>
+            <a className="navbar-brand"  >S.G.A.T.</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav">
                     <li className="nav-item ">
-                        <a className="nav-link" href="#">Inicio<span className="sr-only"></span></a>
-                    </li>
-                    <li className="nav-item ">
-                        <a className="nav-link" href="#">Talleres</a>
+                        <a className="nav-link" >Talleres</a>
                     </li>
                     <li className="nav-item dropdown ">
-                        <a className="nav-link dropdown-toggle" href="#"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Inscribir</a>
+                        <a className="nav-link dropdown-toggle"   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Inscribir</a>
                         <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a className="dropdown-item" href="#">Pre-Inscripto</a>
-                            <a className="dropdown-item" href="#" onClick={() => this.mostrarNuevoAlumno()}>Nuevo Alumno</a>
+                            <a className="dropdown-item"  onClick={() => this.mostrarNuevoAlumno()}>Nuevo Alumno</a>
                             {/*TODO esto es temporal*/}
-                            <a className="dropdown-item" href="#" onClick={() => this.mostrarListarAlumno()}>Listar Alumnos temp</a>
+                            <a className="dropdown-item"  onClick={() => this.mostrarListarAlumno()}>Listar Alumnos temp</a>
                         </div>
                     </li>
                     <li className="nav-item dropdown active">
-                        <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a className="nav-link dropdown-toggle"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Nuevo
                         </a>
                         <div className="dropdown-menu" aria-labelledby="#">
-                        <a className="dropdown-item" href="#" onClick={() =>this.mostrarNuevoTaller()}>Taller</a>
-                        <a className="dropdown-item" href="#">Curso</a>
+                        <a className="dropdown-item"  onClick={() =>this.mostrarNuevoTaller()}>Taller</a>
+                        <a className="dropdown-item" >Curso</a>
                         </div>
                     </li>
                 </ul>
