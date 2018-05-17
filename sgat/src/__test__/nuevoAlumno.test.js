bfd95bfe64a664dbf7ea6722eb19b0bf08997569
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-05-17 18:03:40 -0300

    en proceso

---
+++ sgat/src/__test__/nuevoAlumno.test.js
@@ -0,0 +1,18 @@
+const React = require('react');
+const Enzyme =  require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+const mockAxios = require('jest-mock-axios');
+const shallow = Enzyme.shallow
+const mount = Enzyme.mount
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const {InputPersona} = require('../nuevoAlumno');
+
+
+describe("React input Persona", () => {
+    const inputPersona = shallow(<InputPersona/>)
+    it("e1", () => {
+        
+    })
+})
