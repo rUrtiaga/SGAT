fff003ee652d23f68e965c9d1620af413ef18cfd
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-05-27 12:55:08 -0300

    corregido problema que no guardaba telSecundario y comentario

--- sgat/server/controller.js
+++ sgat/server/controller.js
@@ -14,6 +14,8 @@ class Controller{
 
     postPersona(dataPersona){
         let persona = new dominio.Persona(dataPersona._dni,dataPersona._nombre,dataPersona._apellido,dataPersona._fechaNac,dataPersona._direccion,dataPersona._telPrincipal,dataPersona._mail)
+        persona.setTelSecundario(dataPersona._telSecundario)
+        persona.setComentario(dataPersona._comentario)
         store.agregarPersona(persona)
     }
 
