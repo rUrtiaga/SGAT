9031ab13095dc81a546bd7e02c2c55349dee4e41
Santiago <sntgdeandrea@gmail.com>
2018-06-02 11:21:32 -0300

    corregidos errores generados en el merge

--- sgat/server/controller.js
+++ sgat/server/controller.js
@@ -1,42 +1,58 @@
 const {dominio} = require('./dominio/dominio-talleres.js')
 const {store} = require('./dominio/dominio-talleres.js')
 
-class Controller {
+function talleresToJSON(talleres){
+   return (JSON.stringify(talleres, function( key, value) {
+        if(key == '_taller') {
+            //TODO cambiar por id, o re ver la estructura
+          return value._nombre;
+        } else {
+          return value;
+        };
+    }))
+}
 
-    getPersonaDNI(dni) {
+class Controller{
+
+    getPersonaDNI(dni){
         //this.verificarDNI(dni)
-        if (store.estaPersonaDNI(dni)) {
+        if(store.estaPersonaDNI(dni)){
             return store.getPersonaDNI(dni)
         } else {
             throw new Error('El dni no se encuentra')
         }
     }
 
-    postPersona(dataPersona) {
-        let persona = new dominio.Persona(dataPersona._dni, dataPersona._nombre, dataPersona._apellido, dataPersona._fechaNac, dataPersona._direccion, dataPersona._telPrincipal, dataPersona._mail)
-        .persona.setTelSecundario(dataPersona._telSecundario)
+    postPersona(dataPersona){
+        let persona = new dominio.Persona(dataPersona._dni,dataPersona._nombre,dataPersona._apellido,dataPersona._fechaNac,dataPersona._direccion,dataPersona._telPrincipal,dataPersona._mail)
+        persona.setTelSecundario(dataPersona._telSecundario)
         persona.setComentario(dataPersona._comentario)
         store.agregarPersona(persona)
     }
 
-    getCategorias() {
+    getCategorias(){
         return store.getCategorias()
     }
 
-    agregarCategoria(categoria) {
+    getTalleres(){
+        return talleresToJSON(store.getTalleres())
+    }
+    getTalleresDe(categoria){
+       let talleresFiltrados = store.getTalleres().filter(t =>t.getCategoria() == categoria)
+        return talleresToJSON(talleresFiltrados)
+    }
+
+    getTallerID(id){
+        return talleresToJSON([store.getTallerLLamado(id)])
+    }
+
+    getSubCatDeTallerID(id){
+        return talleresToJSON( store.getTallerLLamado(id).getSubCategorias() )
+    }
+  
+    agregarCategoria(categoria){
         store.addCategoria(categoria)
-    }
-
-    agregarTaller(datosTaller) {
-        let unTaller = new dominio.Taller(datosTaller._Categoria, datosTaller._nombre, datosTaller._subCategorias)
-        store.addTaller(unTaller)
-    }
-
-    talleres(categoria){
-        console.log(store.getTalleres())
-        return store.talleresPorCategoria(categoria)
-
     }
 }
 
-exports.controller = new Controller()
+exports.controller = new Controller()    
