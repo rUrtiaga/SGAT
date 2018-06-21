db7841f3ae4b03b07e0454ba0b3c2674fc630b54
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-06-21 19:02:48 -0300

    puestos a v1 los archivos

--- sgat/app.js
+++ sgat/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api');
+var apiRouter = require('./routes/api-v1');
 
 var app = express();
 
