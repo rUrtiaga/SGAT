a0105433705a669ee484b03ddd29899cdfb2c585
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-08-27 17:46:30 -0300

    validaciones del lado del cliente, falta sacar los regex a otro archivo

--- client/src/componentesComunes/inputPersona.jsx
+++ client/src/componentesComunes/inputPersona.jsx
@@ -1,6 +1,7 @@
 const React = require("react");
 const axios = require("axios");
 const { AceptarYCancelar } = require("./botones.jsx");
+const { Input } = require("../componentesComunes/Input.jsx");
 
 class InputPersona extends React.Component {
   constructor(props) {
@@ -16,17 +17,90 @@ class InputPersona extends React.Component {
       telPrincipal: this._persona._telPrincipal,
       telSecundario: this._persona._telSecundario,
       mail: this._persona._mail,
-      comentario: this._persona._comentario
+      comentario: this._persona._comentario,
+      formErrors: {}
     };
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+
+    switch (fieldName) {
+      case "mail":
+        let emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.mail = emailValid
+          ? undefined
+          : "Correo no valido, ingreselo sin espacios y respetando la convension";
+        break;
+      case "dni":
+        let dniValid = value.match(/^\d{7,8}$/i);
+        fieldValidationErrors.dni = dniValid
+          ? undefined
+          : "D.N.I. no valido, ingrese el dni sin puntos, comas o espacios";
+        break;
+      case "nombre":
+        fieldValidationErrors.nombre = !value.match(/[0-9]/i)
+          ? undefined
+          : "nombre no valido";
+        break;
+      case "apellido":
+        fieldValidationErrors.apellido = !value.match(/[0-9]/i)
+          ? undefined
+          : "apellido no valido";
+        break;
+      case "telPrincipal":
+        fieldValidationErrors.telPrincipal = !value.match(/[A-z]/i)
+          ? undefined
+          : "telefono no valido";
+        break;
+      case "telSecundario":
+        fieldValidationErrors.telSecundario =
+          value.match(/^\d+$/i) || value == ""
+            ? undefined
+            : "telefono no valido";
+        break;
+      default:
+        break;
+    }
+    this.setState({ formErrors: fieldValidationErrors });
+  }
+
+  formValid() {
+    return this.requiredFields() && !this.formErrors();
+  }
+
+  formErrors() {
+    const allerrormsg = Object.keys(this.state.formErrors).map(
+      key => this.state.formErrors[key]
+    );
+    let result = false;
+    for (let index = 0; index < allerrormsg.length; index++) {
+      let emsg = allerrormsg[index];
+      result = result || !!emsg;
+    }
+    return result;
+  }
+
+  //Devuelve verdadero (trusty) si los campos requeridos estan completos
+  requiredFields() {
+    return (
+      this.state.dni &&
+      this.state.nombre &&
+      this.state.apellido &&
+      this.state.mail &&
+      this.state.fechaNac
+    );
   }
 
   handleChange(event) {
     try {
-      // this.validate(event.target.name,event.target.value)
+      const name = event.target.name;
+      const value = event.target.value;
       this.setState(
         {
-          [event.target.name]: event.target.value
-        }
+          [name]: value
+        },
+        () => this.validateField(name, value)
       );
     } catch (error) {
       console.log(error);
@@ -72,143 +146,111 @@ class InputPersona extends React.Component {
   toDateUI(isoDate) {
     return isoDate.slice(0, isoDate.indexOf("T"));
   }
-  //esto no va, pero lo dejo por que es algo similar a lo que se va a validar
-  // validate(nameEvent,value){
-  //     switch (nameEvent) {
-  //         case 'dni':
-  //             if(value > 100000000){
-  //                 throw new Error('El dni ingresado es demasiado alto')
-  //             }
-  //             if(value.toString().match(/[^0-9]+$/) ){
-  //                 throw new Error('Ingrese solo numeros')
-  //             }
-  //             break;
-
-  //         default:
-  //             break;
-  //     }
-  // }
 
   render() {
     return (
       <React.Fragment>
         <div className="form-group">
-        <h4 className="mt-4 mb-4" htmlFor="contact">
+          <h4 className="mt-4 mb-4" htmlFor="contact">
             Datos de la Persona
           </h4>
           <div className="form-row">
-            <div className="col-12 col-md-6">
-              <label htmlFor="name">D.N.I.</label>
-              <input
-                type="number"
-                min="3000000"
-                max="99999999"
-                className="form-control"
-                name="dni"
-                placeholder="D.N.I."
-                value={this.state.dni}
-                onChange={event => this.handleDNI(event)}
-              />
-              <small id="dniHelp" className="form-text text-muted">
-                Ingrese el DNI sin puntos.
-              </small>
-            </div>
-            <div className="col-12 col-md-6">
-              <label>Fecha de Nacimiento</label>
-
-              <input
-                className="form-control"
-                type="date"
-                name="fechaNac"
-                value={this.state.fechaNac}
-                onChange={event => this.handleChange(event)}
-                id="date-input"
-              />
-            </div>
+            <Input
+              divClass="col-12 col-md-6"
+              htmlFor="dni"
+              label="D.N.I."
+              type="number"
+              name="dni"
+              value={this.state.dni}
+              onChange={event => this.handleDNI(event)}
+              errorMsg={this.state.formErrors.dni}
+              min="1000000"
+              max="99999999"
+            />
+            <Input
+              divClass="col-12 col-md-6"
+              label="Fecha de Nacimiento"
+              htmlFor="date"
+              type="date"
+              name="fechaNac"
+              value={this.state.fechaNac}
+              onChange={event => this.handleChange(event)}
+              errorMsg={this.state.formErrors.fechaNac}
+              min={this.fecha(130)}
+              max={this.fecha(3)}
+            />
           </div>
         </div>
-
         <div className="form-group">
           <div className="form-row">
-            <div className="col-12 col-md-6">
-              <label htmlFor="name">Nombre</label>
-              <input
-                type="text"
-                className="form-control"
-                name="nombre"
-                placeholder="introduzca Nombre"
-                value={this.state.nombre}
-                onChange={event => this.handleChange(event)}
-              />
-            </div>
-            <div className="col-12 col-md-6">
-              <label htmlFor="lastname">Apellido</label>
-              <input
-                type="text"
-                className="form-control"
-                name="apellido"
-                placeholder="introduzca Apellido"
-                value={this.state.apellido}
-                onChange={event => this.handleChange(event)}
-              />
-            </div>
+            <Input
+              divClass="col-12 col-md-6"
+              label="Nombre"
+              htmlFor="name"
+              type="text"
+              name="nombre"
+              value={this.state.nombre}
+              onChange={event => this.handleChange(event)}
+              errorMsg={this.state.formErrors.nombre}
+            />
+            <Input
+              divClass="col-12 col-md-6"
+              label="Apellido"
+              type="text"
+              name="apellido"
+              placeholder="introduzca Apellido"
+              value={this.state.apellido}
+              onChange={event => this.handleChange(event)}
+              errorMsg={this.state.formErrors.apellido}
+            />
           </div>
         </div>
-
         <div className="form-group">
           <h5 className="mt-4 mb-4" htmlFor="contact">
             Contacto
           </h5>
 
-          <div className="form-group">
-            <label htmlFor="direc">Dirección</label>
-            <input
-              type="text"
-              className="form-control"
-              name="direccion"
-              placeholder="ingrese direccion"
-              value={this.state.direccion}
+          <Input
+            label="Dirección"
+            type="text"
+            name="direccion"
+            placeholder="ingrese dirección"
+            value={this.state.direccion}
+            onChange={event => this.handleChange(event)}
+            errorMsg={this.state.formErrors.direccion}
+          />
+          <div className="form-row">
+            <Input
+              divClass="col-12 col-md-6"
+              label="Teléfono Principal"
+              type="number"
+              name="telPrincipal"
+              placeholder="formato: 0224345XXXX"
+              value={this.state.telPrincipal}
               onChange={event => this.handleChange(event)}
+              errorMsg={this.state.formErrors.telPrincipal}
+            />
+            <Input
+              divClass="col-12 col-md-6"
+              label="Teléfono Secundario"
+              type="number"
+              name="telSecundario"
+              placeholder="formato: 0224345XXXX"
+              value={this.state.telSecundario}
+              onChange={event => this.handleChange(event)}
+              errorMsg={this.state.formErrors.telSecundario}
             />
           </div>
-          <div className="form-row">
-            <div className="col-12 col-md-6">
-              <label htmlFor="pPhone">Telefono Principal</label>
-              <input
-                type="number"
-                className="form-control"
-                name="telPrincipal"
-                placeholder="formato: 0224345XXXX"
-                value={this.state.telPrincipal}
-                onChange={event => this.handleChange(event)}
-              />
-            </div>
-            <div className="col-12 col-md-6">
-              <label htmlFor="sPhone">Secundario</label>
-              <input
-                type="number"
-                className="form-control"
-                name="telSecundario"
-                placeholder="formato: 0224345XXXX"
-                value={this.state.telSecundario}
-                onChange={event => this.handleChange(event)}
-              />
-            </div>
-          </div>
         </div>
-
-        <div className="form-group">
-          <label htmlFor="exampleFormControlInput1">Correo Electronico</label>
-          <input
-            type="email"
-            className="form-control"
-            name="mail"
-            placeholder="nombre@dominio.com"
-            value={this.state.mail}
-            onChange={event => this.handleChange(event)}
-          />
-        </div>
-
+        <Input
+          label="Correo electrónico"
+          htmlFor="mail"
+          type="email"
+          name="mail"
+          value={this.state.mail}
+          onChange={event => this.handleChange(event)}
+          errorMsg={this.state.formErrors.mail}
+        />
         <div className="form-group">
           <label htmlFor="exampleFormControlTextarea1">Comentario</label>
           <textarea
@@ -219,12 +261,13 @@ class InputPersona extends React.Component {
             onChange={event => this.handleChange(event)}
           />
         </div>
-        
-        <AceptarYCancelar acceptText={"Aceptar"} cancelText={"Cancelar"}
-          aceptar={(alert) => this.aceptarPersona(alert)} 
+        <AceptarYCancelar
+          acceptText={"Aceptar"}
+          cancelText={"Cancelar"}
+          disabled={!this.formValid()}
+          aceptar={alert => this.aceptarPersona(alert)}
           cancelar={() => this.cancel()}
         />
-        )}
       </React.Fragment>
     );
   }
@@ -251,6 +294,12 @@ class InputPersona extends React.Component {
     });
   }
 
+  fecha(resta) {
+    let fecha = new Date();
+    let anioRestado = fecha.getFullYear() - resta;
+    return anioRestado + "-" + "01" + "-" + "01";
+  }
+
   aceptarPersona(alert) {
     let self = this;
     const persona = {
@@ -268,12 +317,16 @@ class InputPersona extends React.Component {
       return axios
         .post("/api/personas", persona)
         .then(function(response) {
-            persona._id = response.data.insertedIds[0];
-            self.props.onAccept(persona);
-            alert.success('Se creó correctamente ' + persona._apellido + ' '+ persona._nombre);
+          persona._id = response.data.insertedIds[0];
+          self.props.onAccept(persona);
+          alert.success(
+            "Se creó correctamente " + persona._apellido + " " + persona._nombre
+          );
         })
         .catch(function(error) {
-          alert.error('Falló al crear ' + persona._apellido + ' '+ persona._nombre);
+          alert.error(
+            "Falló al crear " + persona._apellido + " " + persona._nombre
+          );
           console.log(error);
         });
     } else {
