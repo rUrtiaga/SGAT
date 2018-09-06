fe3c732f39e4c6d686480246727a522d3650d972
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-09-06 18:09:13 -0300

    cambio el mensaje de cofirmacion de la generacion del archivo

--- client/src/forBuild/proxyApiGenerator.js
+++ client/src/forBuild/proxyApiGenerator.js
@@ -4,7 +4,9 @@ var process = require('process');
 fs.writeFile("./src/forBuild/proxyApi.js","export default '" + process.env.PROXY_API +"'", function(err) {
     if(err) {
         return console.log(err);
+    } else {
+        
+        console.log("La variable de entorno PROXY_API fue grabada éxitosamente");
     }
 
-    console.log("The file was saved!");
 }); 
