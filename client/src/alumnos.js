328574283d8dfffc16d41aff19747ea102cede2d
sintornillos <rubiancocarlos@gmail.com>
2018-09-15 21:45:50 -0300

    Elimine las lineas comentadas

--- client/src/alumnos.js
+++ client/src/alumnos.js
@@ -7,8 +7,6 @@ var pdfMake = require('pdfmake/build/pdfmake.js');
 var pdfFonts = require('pdfmake/build/vfs_fonts.js');
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-// var mesActual = new Date().getMonth;
-
 /***********************************************
  Alumnos
 ***********************************************/
