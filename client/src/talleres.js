f8e79673c5b88f7957dc371cd4a9ce1e263a3871
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-09-30 04:21:20 -0300

    P Talleres - Menu lateral y contenido central.
    Se generó gran parte de la pantalla que muestra los talleres y sus cursadas,
    se dejó de lado los talleres que no posean cursadas, tambien se sacó codigo
    comentado obsoleto.

--- client/src/talleres.js
+++ client/src/talleres.js
@@ -1,29 +1,107 @@
 const React = require("react");
 const axios = require("axios");
+const _ = require("lodash");
 const { Curso } = require("./componentesComunes/MostrarCurso.jsx");
+
+class MostrarTalleres extends React.Component {
+  render() {
+    return this.props.talleres.map(nombreTaller => {
+      return (
+        <div className="card mb-sm-3 mb-3" key={nombreTaller}>
+          <div className="card-header">
+            <h4 className="card-title">{nombreTaller}</h4>
+          </div>
+          {this.desplegarSubcategorias(nombreTaller)}
+        </div>
+      );
+    });
+  }
+
+  desplegarSubcategorias(nombreTaller) {
+    return this.props.listTalleres
+      .filter(
+        t => t._nombre === nombreTaller && t._categoria === this.props.categoria
+      )
+      .map(tallerDesnormalizado => {
+        return (
+          <div
+            key={tallerDesnormalizado._subCategoria}
+          >
+            <div className="card-body">
+              <h5 className="card-title">
+                {tallerDesnormalizado._subCategoria}
+              </h5>
+              {this.props.desplegarCursosDeTaller(tallerDesnormalizado)}
+            </div>
+          </div>
+        );
+      });
+  }
+}
+
+class ListaCategorias extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h5 className="text-center">Categorias</h5>
+        <ul className="nav flex-column  nav-pills">
+          {this.props.categorias.map(c => (
+            <NavItem
+              categoria={c}
+              extraName={this.props.selected === c ? "active" : ""}
+              select={() => {
+                this.props.select(c);
+              }}
+              key={c}
+            />
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+  selectActive(cat) {}
+}
+
+class NavItem extends React.Component {
+  render() {
+    return (
+      <li className="nav-item">
+        <a
+          className={"nav-link btn " + this.props.extraName}
+          onClick={() => this.props.select()}
+        >
+          {this.props.categoria}
+        </a>
+       </li>
+    );
+  }
+}
 
 class Talleres extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       listaDeCursos: [],
-      listaDeTalleres: []
+      listaDeTalleres: [],
+      listaDeCategorias: []
     };
   }
 
   componentDidMount() {
-    this.getDataTalleres();
-    this.getDataCursos();
+    this.getDataCursos().then(listCursos => this.getDataTalleres(listCursos));
   }
 
-  getDataTalleres() {
+  getDataTalleres(listCursos) {
     let self = this;
     return axios
       .get("/api/talleres")
       .then(function(response) {
-        const listTalleres = response.data;
+        let listTalleres = response.data.filter(t=>listCursos.some(c=>c._tallerID === t._id));
+        let listCategorias = _.sortedUniq(listTalleres.map(t => t._categoria));
         self.setState({
-          listaDeTalleres: listTalleres
+          listaDeTalleres: listTalleres,
+          listaDeCategorias: listCategorias,
+          selectedCategory: listCategorias ? listCategorias[0] : ""
         });
         return Promise.resolve();
       })
@@ -41,7 +119,7 @@ class Talleres extends React.Component {
         self.setState({
           listaDeCursos: listCursos
         });
-        return Promise.resolve();
+        return Promise.resolve(listCursos);
       })
       .catch(function(error) {
         console.log(error);
@@ -61,29 +139,45 @@ class Talleres extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        {this.state.listaDeTalleres.map(tallerDesnormalizado => {
-          return (
-            <div className="card border-0">
-              <div className="card-body">
-                <h3 className="card-title">
-                  {" "}
-                  {tallerDesnormalizado._nombre +
-                    " " +
-                    tallerDesnormalizado._subCategoria}{" "}
-                </h3>
-                {this.desplegarCursosDeTaller(tallerDesnormalizado)}
-              </div>
-            </div>
-          );
-        })}
+      <div className="container pt-4">
+        <div className="row">
+          <div className="col-sm-2 mt-1 mr-1">
+            <ListaCategorias
+              categorias={this.state.listaDeCategorias}
+              select={this.selecCategoria.bind(this)}
+              selected={this.state.selectedCategory}
+            />
+          </div>
+
+          <div className="col-sm-9">
+            <h3 className="text-center">{this.state.selectedCategory}</h3>
+            <MostrarTalleres
+              categoria={this.state.selectedCategory}
+              listTalleres={this.state.listaDeTalleres}
+              talleres={this.nombresTalleres()}
+              desplegarCursosDeTaller={t => this.desplegarCursosDeTaller(t)}
+            />
+          </div>
+        </div>
       </div>
+    );
+  }
+
+  selecCategoria(cat) {
+    this.setState({ selectedCategory: cat });
+  }
+
+  nombresTalleres() {
+    return _.sortedUniq(
+      this.state.listaDeTalleres
+        .filter(t => t._categoria === this.state.selectedCategory)
+        .map(t => t._nombre)
     );
   }
 
   desplegarCursosDeTaller(tdsn) {
     return this.state.listaDeCursos
-      .filter(c => c._tallerID == tdsn._id)
+      .filter(c => c._tallerID === tdsn._id)
       .map(curso => (
         <Curso
           key={curso._id}
@@ -97,44 +191,6 @@ class Talleres extends React.Component {
         />
       ));
   }
-  /**
-   * 
-
-   */
-  // tblTalleres(){
-  // 	return(
-  // 		<table className = "table table-striped" >
-  // 			<thead>
-  // 				<tr>
-  // 					{this.encabezadoDeLaTabla(["Talleres"])}
-  // 				</tr>
-  // 			</thead>
-  // 			<tbody className="table table-bordered">
-  // 				{ this.state.listaDeTalleres.map(taller => this.nombreDeTaller(taller)) }
-  // 			</tbody>
-  // 		</table>
-  // 	)
-  // }
-
-  /** --- Datos de la Tabla --- */
-  // Aca completo la tabla con los datos del taller
-  // nombreDeTaller(unTaller){
-  // 	console.log(unTaller);
-  // 	const rowDatosDelTaller = (
-  // 		<tr id="infoTaller" key={unTaller._nombre}>
-  // 			<td>{unTaller._nombre}</td>
-  // 			<td>{unTaller._categoria}</td>
-  // 			<td>{unTaller._subCategoria}</td>
-  // 		</tr>
-  // 	)
-  // 	return rowDatosDelTaller
-  // }
-
-  /** --- Encabezado de la Tabla --- */
-  // Acá le doy formato a los titulos de la tabla
-  // encabezadoDeLaTabla(titulos) {
-  // 	return titulos.map((titulo, x) => (<th key={x}>{titulo}</th>))
-  // }
 }
 
 class Botones extends React.Component {
@@ -143,13 +199,13 @@ class Botones extends React.Component {
       <React.Fragment>
         <button className="col-md-3 btn btn-primary mr-1 mb-1">Espera</button>
         <button
-          className="btn btn-primary col-md-3 mr-1 mb-1"
+          className="btn btn-primary col-md-4 mr-1 mb-1"
           onClick={v => this.props.seleccionarAlumnos(v)}
         >
           Alumnos
         </button>
         <button
-          className="btn btn-primary col-md-3 mb-1"
+          className="btn btn-primary col-md-4 mb-1"
           onClick={v => this.props.inscribirAlumno(v)}
         >
           Inscribir
