8bf44ad25fbdc444ce06609727de6d1bea9a3721
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-10-10 16:52:48 -0300

    fix eliminar subcategorias

--- client/src/componentesComunes/MuestraSubCategoria.jsx
+++ client/src/componentesComunes/MuestraSubCategoria.jsx
@@ -17,56 +17,57 @@ class MuestraSubCategoria extends React.Component {
     this.setState({ mostrarEditar: false });
   }
 
-
   mostrarSubCategoria() {
-      return (
-        <div className="form-row mb-2 mt-2">
-          <div className="col-md-2">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="SubCategoria"
-              value={this.state.nombreSubCategoria}
-              onChange={event =>
-                this.setState({ nombreSubCategoria: event.target.value })
-              }
-            />
-          </div>
-          <div className="col-md-1">
-            <button
-              type="button"
-              className="btn btn-success"
-              onClick={() => this.guardar()}
-            >
-              <span className="fa fa-check"> </span>
-            </button>
-          </div>
+    return (
+      <div className="form-row mb-2 mt-2">
+        <div className="col-md-2">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="SubCategoria"
+            value={this.state.nombreSubCategoria}
+            onChange={event =>
+              this.setState({ nombreSubCategoria: event.target.value })
+            }
+          />
         </div>
-      );
+        <div className="col-md-1">
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => this.guardar()}
+          >
+            <span className="fa fa-check"> </span>
+          </button>
+        </div>
+      </div>
+    );
   }
 
   render() {
     return (
       <div>
-        {(this.state.mostrarEditar === false)?
-        <div> 
-        <button
-          className="btn btn-link"
-          onClick={() => this.mostrarModificarSubCategoria()}
-        >
-          {this.state.nombreSubCategoria}
-        </button>
-        <button
-            type="button"
-            className="btn btn-link"
-            onClick={() => this.props.padre.quitarSubCategoria(this.state.nombreSubCategoria)}
-        >
-            <span className="fa fa-trash"> </span>
-        </button>
-        </div> 
-        :
-        this.mostrarSubCategoria()
-        }
+        {this.state.mostrarEditar === false ? (
+          <div>
+            <button
+              className="btn btn-link"
+              onClick={() => this.mostrarModificarSubCategoria()}
+            >
+              {this.state.nombreSubCategoria}
+            </button>
+            <button
+              type="button"
+              className="btn btn-link"
+              onClick={() =>
+                this.props.quitarSubCategoria(this.state.nombreSubCategoria)
+              }
+            >
+              <span className="fa fa-trash"> </span>
+            </button>
+          </div>
+        ) : (
+          this.mostrarSubCategoria()
+        )}
       </div>
     );
   }
