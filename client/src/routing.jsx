13e50e8db6f2d0512f1d553fee4e7b67dc7e0500
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-11-09 17:49:51 -0300

    pantalla de lista de espera y consulta para eliminar de espera

--- client/src/routing.jsx
+++ client/src/routing.jsx
@@ -5,6 +5,7 @@ import { NuevoAlumno } from "./nuevoAlumno";
 import { NuevoCurso, EditarCurso } from "./nuevoCurso";
 import { CrearTaller, EditarTaller } from "./nuevoTallerReact";
 import { ListarAlumnos } from "./alumnos";
+import { ListaEspera } from "./listaEspera";
 
 class Routing extends Component {
   render() {
@@ -14,6 +15,7 @@ class Routing extends Component {
         <Route exact path="/talleres/" component={Talleres} />
         <Route exact path="/agregarAlumno" component={NuevoAlumno} />
         <Route exact path="/alumnos/" component={ListarAlumnos} />
+        <Route exact path="/listaEspera/" component={ListaEspera} />
         <Route path="/nuevaCursada/" component={NuevoCurso} />
         <Route path="/editarCursada/" component={EditarCurso} />
         <Route path="/nuevoTaller/" component={CrearTaller} />
