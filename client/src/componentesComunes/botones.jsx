820bfd5bbc754af4856ddca0f754492bb4ab42cb
Ramiro <ramiro.urtiaga@alu.unq.edu.ar>
2018-11-20 18:23:58 -0300

    removido codigo de mal merge

--- client/src/componentesComunes/botones.jsx
+++ client/src/componentesComunes/botones.jsx
@@ -40,25 +40,11 @@ class BackButton extends React.Component {
       <button
         className={"btn btn-success mr-3 "}
         onClick={this.props.history.goBack}
-<<<<<<< HEAD
-<<<<<<< HEAD
       >
         <span className={"fa fa-arrow-circle-left"}>Atrás</span>
-=======
-=======
->>>>>>> Botones en listarAlumnos
-        >
-        <span className={"fa fa-arrow-circle-left"}>
-          Atrás
-        </span>
-<<<<<<< HEAD
->>>>>>> Boton inscribir alumno en listado de Alumnos
-=======
->>>>>>> Botones en listarAlumnos
       </button>
     );
   }
-
 }
 
 exports.BackButton = BackButton;
